07fadaff880f9c69a800ded33fd0383461a78da9
Zhu Zhanyan <program.nom@gmail.com>
2021-07-18 15:06:36 +0800

    feat(exercises): Add solution to Exercise 2.4.5.2: Square Dance!

---
+++ src/test/scala/chapter_2/SquareDanceTest.scala
@@ -0,0 +1,18 @@
+/*
+ * essential-scala
+ * exercise 2.4.5.2: Square Dance!
+*/
+
+package co.mrzzy.essentialScala.chapter2;
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class SquareDanceTest extends AnyFlatSpec  {
+  behavior of "SquareDance.calc"
+
+  it should "compute the square of given x" in {
+    assert((for(i <- 1 to 20) yield i).forall(
+      i => SquareDance.calc.square(i) == i * i
+    ))
+  }
+}
