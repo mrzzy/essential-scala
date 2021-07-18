b69775f8ca8c1d4bfa90842a507195c3d758b0c1
Zhu Zhanyan <program.nom@gmail.com>
2021-07-18 15:39:31 +0800

    feat(exercise): Add solutions for Exercise 2.4.5.3: Precise Square Dance

---
+++ src/main/scala/chapter_2/PreciseSquareDance.scala
@@ -0,0 +1,15 @@
+/*
+ * essential-scala
+ * exercise 2.4.5.3: Precise Square Dance!
+*/
+
+package co.mrzzy.essentialScala.chapter2;
+
+object PreciseSquareDance {
+  object calc {
+    // use method overloading to provide implementations for both Double & Int
+    def square(x: Double) = x * x
+    def square(x: Int) = x * x
+  }
+}
+
---
+++ src/test/scala/chapter_2/PreciseSquareDanceTest.scala
@@ -0,0 +1,21 @@
+/*
+ * essential-scala
+ * exercise 2.4.5.3: Precise Square Dance!
+*/
+
+package co.mrzzy.essentialScala.chapter2;
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class PreciseSquareDanceTest extends AnyFlatSpec  {
+  behavior of "PreciseSquareDance.calc"
+
+  it should "compute the square of given Double x" in {
+    assert(PreciseSquareDance.calc.square(2.4) == 2.4 * 2.4)
+  }
+
+
+  it should "compute the square of given Int x" in {
+    assert(PreciseSquareDance.calc.square(2) == 2 * 2)
+  }
+}
